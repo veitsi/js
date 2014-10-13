a44aea5c456f8a2ede495195e226632c2a1a01f6
xtfkpi <xtfkpi@yandex.ru>
2014-10-13 09:10:19 +0300

    function is developing

--- rps.js
+++ rps.js
@@ -1,1 +1,20 @@
-var userChoice=prompt("Do you choose rock, paper or scissors?");
+var  compare = function (choice1 ,choice2)
+{
+    if (choice1 === choice2) return "The result is a tie!";
+    else if (choice1 === "rock")
+    {
+        if (choice2 === "scissors") 
+        return "rock wins";
+        else return "paper wins"
+    }
+}
+/*var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+	computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+	computerChoice = "paper";
+} else {
+	computerChoice = "scissors";
+} console.log("Computer: " + computerChoice);*/
+
